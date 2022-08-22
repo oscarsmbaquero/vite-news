552dc63a9402ba4bdba0a9bdfbced8f08461b4cf
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-08-22 18:10:29 +0200

    cambio url

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -11,7 +11,7 @@ const NoticiasProvider = ({children}) => {
 
     useEffect(() => {
         const consultarAPI = async () => {
-            const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+            const url = `https://newsapi.org/v2/top-headlines?country=es&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
 
             const { data } = await axios(url)
             setNoticias(data.articles)
@@ -23,7 +23,7 @@ const NoticiasProvider = ({children}) => {
 
     useEffect(() => {
         const consultarAPI = async () => {
-            const url = `https://newsapi.org/v2/top-headlines?country=mx&page=${pagina}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+            const url = `https://newsapi.org/v2/top-headlines?country=es&page=${pagina}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
 
             const { data } = await axios(url)
             setNoticias(data.articles)
